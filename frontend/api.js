2e0fe9d8efad0ed68e2a7a80be2923a4dc966b9f
junior-pro-256 <wafutujunior7@gmail.com>
2026-09-21 20:51:22 +0300

    Change API URL to production endpoint

--- frontend/api.js
+++ frontend/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://lets-chat-9dy1.onrender.com';
 
 async function apiRequest(endpoint, method = 'GET', body = null, isFormData = false) {
   const token = localStorage.getItem('token');
